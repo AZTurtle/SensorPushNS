d3c605b4d1f339b1bb80b3eb7f544e4327646195
AZTurtle <jamesbennett@azturtle.com>
2023-07-27 04:05:50 -0700

    update

--- nodes/gateway.py
+++ nodes/gateway.py
@@ -33,18 +33,6 @@ class Controller(udi_interface.Node):
         self.count = 0
         self.n_queue = []
 
-        polyglot.subscribe(polyglot.CUSTOMTYPEDPARAMS, self.customParamHandler)
-
-        self.CustomTypedParams = Custom(polyglot, 'customtypedparams')
-        self.CustomParams = Custom(polyglot, 'customtypeddata')
-
-        params = [
-            {name: 'email', title: 'E-mail', isRequired: true},
-            {name: 'password', title: 'Password', isRequired: true},
-            {name: 'gateways', title: 'Gateways', isRequired: true, isList: true}
-        ]
-
-        self.CustomTypedParams.load(params)
 
         self.Parameters = Custom(polyglot, 'customparams')
 
@@ -94,10 +82,6 @@ class Controller(udi_interface.Node):
             self.poly.Notices.clear()
         else:
             self.poly.Notices['nodes'] = 'Please configure the number of child nodes to create.'
-
-    def customParamHandler(self, params):
-        
-        self.CustomParams.load(params)
 
     '''
     This is called when the node is added to the interface module. It is
